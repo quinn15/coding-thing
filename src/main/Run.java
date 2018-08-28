3d3f724b6b228ad5653273bea4393013286cf563
Quinny <Quinny@192.168.0.43>
2018-08-27 18:27:17 -0600

    just messing around

--- src/main/Run.java
+++ src/main/Run.java
@@ -2,10 +2,23 @@ package main;
 
 public class Run {
 
+	public static String word = "pneumonoultramicroscopicsilicovolcanoconiosis";
+	public static int paradox = 6;
+	
 	public static void main(String[] args) {
-		
-		System.out.println("Lol");
-		
+		say(word);
+		word = "team fortress 2";
+		say(word);
+		System.out.println(paradox);
+		System.out.println(add(paradox));
+	}
+	
+	public static void say(String in) {
+		System.out.println(in);
+	}
+	
+	public static int add(int zello) {
+		return zello + 1;
 	}
 	
 }
